d1199abe282ef1189e590f712fb376de1e6a40c8
HIBIKI <hibiki.apple.1984@gmail.com>
2025-02-07 01:41:28 +0900

    Mod: 発表用に調整

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,5 +1,5 @@
-import { relations, sql } from 'drizzle-orm';
-import { blob, integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable } from 'drizzle-orm/sqlite-core';
 
 export const count = sqliteTable('Count', {
   id: integer().primaryKey({ autoIncrement: true }),
